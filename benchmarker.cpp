b4dad24d7183b70c2ca8ef63e89f71428ce209d4
Matthew Nielsen <md2999ni@edu.uwaterloo.ca>
2018-03-24 19:57:58 -0400

    update code documentation

--- benchmarker.cpp
+++ benchmarker.cpp
@@ -1,10 +1,20 @@
+//////////////////////////////////////////////////////////////
+// Title: Benchmarker ////////////////////////////////////////
+// Description: C++ inline code benchmarking library /////////
+// Written by: Matthew Nielsen ///////////////////////////////
+// Date: March 24 2018 ///////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
 #include <iostream>
 #include "benchmarker.hpp"
 
+//  Constructor
 Benchmarker::Benchmarker(){
   started = false;
 }
 
+//  To start the benchmarker with a certain title
+//  Returns false if benchmark is currently underway
 bool Benchmarker::start(std::string title){
   using namespace std::chrono;
 
@@ -22,6 +32,8 @@ bool Benchmarker::start(std::string title){
   }else return false;
 }
 
+//  To add a checkpoint with a certain title
+//  Returns false if no benchmark is underway
 bool Benchmarker::checkpoint(std::string title){
   using namespace std::chrono;
 
@@ -37,6 +49,9 @@ bool Benchmarker::checkpoint(std::string title){
   }else return false;
 }
 
+//  To end the benchmark. If display_results is true
+//  benchmark results will be printed to the console.
+//  Returns false if no benchmark is underway
 bool Benchmarker::end(bool display_results){
   using namespace std::chrono;
 
@@ -50,26 +65,30 @@ bool Benchmarker::end(bool display_results){
   }else return false;
 }
 
+// Print current benchmark results to the console
 void Benchmarker::display(){
   using namespace std::chrono;
-  std::string name;
 
+  std::string name;
   std::string header = "--------------------BENCHMARK RESULTS";
 
+  //  Benchmark header
   header.resize(66, '-');
-
   std::cout << header << "\n";
 
   for(int x = 0; x < breakpoints.size(); x++){
+    //Retrieve and format current title name
     name = breakpoints[x];
-
     name.resize(50,' ');
+
     if(x == 0){
+      //  Benchmark start
       std::cout 
         << name 
         << "0.0 seconds\n";
 
     }else if(x == breakpoints.size() - 1){
+      // Last checkpoint
       duration<double> time_span = 
         duration_cast<duration<double> >(times[x] - times[x - 1]);
 
@@ -78,6 +97,7 @@ void Benchmarker::display(){
         << time_span.count() 
         << " seconds\n";
 
+      //  Total elapsed time
       time_span = 
         duration_cast<duration<double> >(times[x] - times[0]);
 
@@ -90,6 +110,7 @@ void Benchmarker::display(){
         << " seconds\n";
 
     }else{
+      //  Checkpoints
       duration<double> time_span = 
         duration_cast<duration<double> >(times[x] - times[x - 1]);
 
@@ -101,6 +122,7 @@ void Benchmarker::display(){
     }
   }
 
+  //  Benchmark footer
   header = "";
   header.resize(66, '-');
 
--- benchmarker.hpp
+++ benchmarker.hpp
@@ -1,18 +1,45 @@
+//////////////////////////////////////////////////////////////
+// Title: Benchmarker ////////////////////////////////////////
+// Description: C++ inline code benchmarking library /////////
+// Written by: Matthew Nielsen ///////////////////////////////
+// Date: March 24 2018 ///////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
+#ifndef BENCHMARKER_HPP
+#define BENCHMARKER_HPP
+
 #include <string>
 #include <vector>
 #include <chrono>
 
 class Benchmarker{
   private:
+  //  Vectors to hold breakpoints and times
   std::vector<std::string> breakpoints;
   std::vector<std::chrono::high_resolution_clock::time_point> times;
 
+  //  If the benchmark is underway
   bool started;
 
   public:
+  //  Constructor
   Benchmarker();
+
+  //  To start the benchmarker with a certain title
+  //  Returns false if benchmark is currently underway
   bool start(std::string title);
+
+  //  To add a checkpoint with a certain title
+  //  Returns false if no benchmark is underway
   bool checkpoint(std::string title);
+
+  //  To end the benchmark. If display_results is true
+  //  benchmark results will be printed to the console.
+  //  Returns false if no benchmark is underway
   bool end(bool display_results);
+
+  // Print current benchmark results to the console
   void display();
 };
+
+#endif
